dd5749b445ceee2f26a1f4b7e1cef0b661af6e54
Tomasz Forys <tomasz.forys@contractors.roche.com>
2023-12-13 12:51:21 +0100

    Exception logging

--- spring-boot-404/src/main/java/infoshare/springboot404/handler/GlobalExceptionHandler.java
+++ spring-boot-404/src/main/java/infoshare/springboot404/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package infoshare.springboot404.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public String globalExceptionHandler(Exception ex, WebRequest request) {
+        log.error(ex.getMessage(), ex);
         return ex.getMessage();
     }
 
